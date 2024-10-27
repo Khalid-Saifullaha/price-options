900b5c5c2b382b0043218dd6ad8913d6b3d87a9a
Your Name <khalidsaifullaha3@gmail.com>
2024-10-27 21:18:40 +0600

    navbar add

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import DaisiUi from './assets/components/DaisiUI/DaisiUi'
+import Navbar from './assets/components/Navbar/Navbar'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar></Navbar>
+    {/* <DaisiUi></DaisiUi> */}
+      
+     
+ 
+
+
+
+    </>
+  )
+}
+
+export default App
