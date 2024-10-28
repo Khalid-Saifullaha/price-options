0c66d3a77abe6b0ba090cf87d0a30686b106d578
Your Name <khalidsaifullaha3@gmail.com>
2024-10-28 11:43:08 +0600

    add line chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
 import './App.css'
 import DaisiUi from './assets/components/DaisiUI/DaisiUi'
+import LineChart from './assets/components/LineChart/LineChart'
 import Navbar from './assets/components/Navbar/Navbar'
+import Phones from './assets/components/Phones/Phones'
 import PriceOptions from './assets/components/PriceOptionse/PriceOptions'
 
 
@@ -13,6 +15,8 @@ function App() {
     <Navbar></Navbar>
     {/* <DaisiUi></DaisiUi> */}
     <PriceOptions></PriceOptions>
+    <LineChart></LineChart>
+    <Phones></Phones>
       
      
  
